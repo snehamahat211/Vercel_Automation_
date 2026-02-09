9442e3fd901902ac04be57a7da79b52ea536204c
snehamahat211 <snehamahat211@gmail.com>
2026-02-09 21:21:53 +0545

    setup for vercel completed

---
+++ src/main/java/signup/Homepage.java
@@ -0,0 +1,4 @@
+package signup;
+
+public class Homepage {
+}
---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import signup.Homepage;
+
+public class BaseTests {
+    private WebDriver driver;
+    protected Homepage homepage;
+
+    @Test
+
+    public void setup()
+    {
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://authorized-partner.vercel.app/");
+//        homepage = new Homepage(driver);
+    }
+
+}
