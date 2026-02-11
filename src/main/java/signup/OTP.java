35b5baefa0f1adea0714828fe0d90d7f0454610a
snehamahat211 <snehamahat211@gmail.com>
2026-02-11 07:42:18 +0545

    waitformail completed

--- src/main/java/signup/OTP.java
+++ src/main/java/signup/OTP.java
@@ -2,6 +2,8 @@ package  signup;
 
 import com.mailosaur.MailosaurClient;
 import com.mailosaur.MailosaurException;
+import com.mailosaur.models.MessageSearchParams;
+import com.mailosaur.models.SearchCriteria;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.http.Message;
@@ -9,6 +11,7 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class OTP {
@@ -18,25 +21,34 @@ public class OTP {
     String serverId="";
     String serverDomain="";
 
-    public Message waitforEmail (String emailid, MailosaurClient mailosaur)throws MailosaurException {
-        Wait<MailosaurClient> wait=new FluentWait<>(mailosaur)
-                .withTimeout(Duration.ofSeconds(30))
-                .pollingEvery(Duration.ofMillis(100))
-                .ignoring(Exception.class);
-
-
-    }
-
-
-
-
-
     private By codebox=By.cssSelector("input[data-input-otp='true']");
 
     public OTP(WebDriver driver) {
         this.driver=driver;
         this.wait=new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
+    public Message waitforEmail (String emailid, MailosaurClient mailosaur)throws MailosaurException {
+        Wait<MailosaurClient> wait=new FluentWait<>(mailosaur)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(100))
+                .ignoring(Exception.class);
+        return wait.until(mailosaurClient->{
+            try{
+                MessageSearchParams params=new MessageSearchParams();
+                params.withServer(serverId);
+
+                SearchCriteria criteria=new SearchCriteria();
+                criteria.withSentTo(emailid);
+
+                Message message = (Message) mailosaurClient.messages().get(params,criteria);
+                return message;}
+            catch (MailosaurException | IOException e){
+                return null;
+            }
+        });
+    }
+
 
 
 
