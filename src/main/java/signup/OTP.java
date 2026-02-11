fe6ade4e6a750180a744c601cc2810f6bdaf9e31
snehamahat211 <snehamahat211@gmail.com>
2026-02-11 07:31:29 +0545

    fluent wait

--- src/main/java/signup/OTP.java
+++ src/main/java/signup/OTP.java
@@ -1,7 +1,12 @@
 package  signup;
 
+import com.mailosaur.MailosaurClient;
+import com.mailosaur.MailosaurException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.http.Message;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,6 +17,15 @@ public class OTP {
     String apikey="";
     String serverId="";
     String serverDomain="";
+
+    public Message waitforEmail (String emailid, MailosaurClient mailosaur)throws MailosaurException {
+        Wait<MailosaurClient> wait=new FluentWait<>(mailosaur)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(100))
+                .ignoring(Exception.class);
+
+
+    }
 
 
 
